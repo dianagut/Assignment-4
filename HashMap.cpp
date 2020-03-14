6dd823166e21981856095b080aff55b8fd1efa16
Andrea <andreashirley7@gmail.com>
2020-03-13 23:50:49 -0700

    Add files via upload

--- HashMap.cpp
+++ HashMap.cpp
@@ -1,11 +1,11 @@
 // ------------------------------------------------HashMap.cpp -------------------------------------------------------
 // Andrea Shirley-Bellande & Diana Gutierrez , 343C
 // Created: March 6, 2020
-// Last Modified:
+// Last Modified: March 14, 2020
 // --------------------------------------------------------------------------------------------------------------------
-// Purpose: 
+// Purpose: This is the header file to hashmap. Hashmap is uses templates
 // --------------------------------------------------------------------------------------------------------------------
-// Notes on specifications, special algorithms, and assumptions. 
+// Assumptions: files will be formatted correctly
 // -------------------------------------------------------------------------------------------------------------------- 
 
 #include "HashMap.h"
